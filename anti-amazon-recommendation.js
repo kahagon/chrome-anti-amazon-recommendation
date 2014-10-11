7dd5193e417ffc7c18ec1d9446651f5b081b3ee0
k.ahagon <kenichi.ahagon@m.gnsj.co>
2014-10-11 22:54:13 +0900

    modified script to wait for xhr response

--- anti-amazon-recommendation.js
+++ anti-amazon-recommendation.js
@@ -1,9 +1,11 @@
 jQuery(function() {
     console.log("anti-amazon-recommendation loaded");
     jQuery(".s_checkUnmarked").click();
-    var $nextButton = jQuery("#ysMoreResults");
-    console.log("next page " + ($nextButton.length?"exists":"does not exist"));
-    if ($nextButton.length) {
-        window.location.href = $nextButton.parent().attr("href");
-    }
+    setTimeout(function() {
+        var $nextButton = jQuery("#ysMoreResults");
+        console.log("next page " + ($nextButton.length?"exists":"does not exist"));
+        if ($nextButton.length) {
+            window.location.href = $nextButton.parent().attr("href");
+        }
+    }, 5000);
 });
